e28c996a172a9c84511734e6dd7a6f9e64b2d97b
Shoxrux061 <shoxruxurmanov2@gmail.com>
2024-10-01 16:54:53 +0500

    Initial commit

---
+++ presentation/src/main/java/com/example/presentation/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.presentation
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val w = window
+        w.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        setFragment()
+
+    }
+
+    private fun setFragment() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        val graph = navHost.navController.navInflater.inflate(R.navigation.main_navigation)
+        navHost.navController.graph = graph
+    }
+}
